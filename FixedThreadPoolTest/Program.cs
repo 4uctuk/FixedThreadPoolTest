649bdf750cbabd35249c0663d5751a6f7809790f
Marat <chuktuk4@gmail.com>
2021-04-25 20:25:01 +0500

    Add project files.

---
+++ FixedThreadPoolTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FixedThreadPoolTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
